308a605a3eebf22a63addc940dc757b07f8cf235
Anouar <anouar.haidary@gmail.com>
2021-02-24 01:11:10 +0100

    Amending footer

--- src/mock/data.js
+++ src/mock/data.js
@@ -54,30 +54,30 @@ export const contactData = {
 };
 
 // FOOTER DATA
-export const footerData = {
-  networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'github',
-      url: '',
-    },
-  ],
-};
+// export const footerData = {
+//   networks: [
+//     {
+//       id: nanoid(),
+//       name: 'twitter',
+//       url: '',
+//     },
+//     {
+//       id: nanoid(),
+//       name: 'codepen',
+//       url: '',
+//     },
+//     {
+//       id: nanoid(),
+//       name: 'linkedin',
+//       url: '',
+//     },
+//     {
+//       id: nanoid(),
+//       name: 'github',
+//       url: '',
+//     },
+//   ],
+// };
 
 // Github start/fork buttons
 export const githubButtons = {
