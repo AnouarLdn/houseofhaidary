2ec4531c72e911c4c75394cc8146e18c648fa2aa
Anouar <anouar.haidary@gmail.com>
2021-03-09 10:28:31 +0100

    Changed Google Analytics Tracker ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
           // The property ID; the tracking code won't be generated without it
-          trackingId: "G-SVDNY2MF4R",
+          trackingId: "G-D29M02CN06",
           // Defines where to place the tracking script - `true` in the head and `false` in the body
           head: true,
           // Setting this parameter is optional
