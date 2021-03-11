93a48d2add7f8097b4ea1432862ac28a055a8e9f
Anouar <anouar.haidary@gmail.com>
2021-03-11 15:31:02 +0100

    Changed title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'House of Haidary, a creative house.'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'House of Haidary specialises in web development'} />
       </Helmet>
       <App />
     </>
